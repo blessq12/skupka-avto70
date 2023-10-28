dc7ca3f881ab4262ca8214f525883c69c563a827
Andrew <code70@inbox.ru>
2023-10-29 02:16:51 +0700

    intermideate commit

--- resources/js/components/RatingRequest.vue
+++ resources/js/components/RatingRequest.vue
@@ -4,7 +4,8 @@ export default{
     data:()=>({
         step: 1,
         requestData:{
-            model: ''
+            model: '',
+            additional:{}
         }
     }),
     computed:{
@@ -13,6 +14,8 @@ export default{
                 case 1: return 'Какой у вас автомобиль?'
                     break;
                 case 2: return 'Основные данные об авто'
+                    break;
+                case 3: return 'Дополнительная информация'
                 default:
                     break;
             }
@@ -76,15 +79,120 @@ export default{
                     <button class="btn btn-primary" @click="step++" :disabled="requestData.brand && requestData.model !== '' ? false : true">Дальше</button>
                 </div>
                 <div v-if="requestData.auto == 'Иномарка'">
-                    иномарка
+                    <div class="form-group mb-2">
+                        <label for="marka">Выберите марку авто</label>
+                        <select name="marka" id="marka" class="form-select" v-model="requestData.brand">
+                            <option selected>Выберите из списка</option>
+                            <option value="Audi">Audi</option>
+                            <option value="BMW">BMW</option>
+                            <option value="Mercedes-Benz">Mercedes-Benz</option>
+                            <option value="Opel">Opel</option>
+                            <option value="Porsche">Porsche</option>
+                            <option value="Volkswagen">Volkswagen</option>
+                            <option value="Daewoo">Daewoo</option>
+                            <option value="Genesis">Genesis</option>
+                            <option value="Hyundai">Hyundai</option>
+                            <option value="Kia">Kia</option>
+                            <option value="SsangYong">SsangYong</option>
+                            <option value="Honda">Honda</option>
+                            <option value="Acura">Acura</option>
+                            <option value="Mazda">Mazda</option>
+                            <option value="Subaru">Subaru</option>
+                            <option value="Infinity">Infinity</option>
+                            <option value="Isuzu">Isuzu</option>
+                            <option value="Mitsubishi">Mitsubishi</option>
+                            <option value="Nissan">Nissan</option>
+                            <option value="Chevrolet">Chevrolet</option>
+                            <option value="Ford">Ford</option>
+                            <option value="Renault">Renault</option>
+                            <option value="Skoda">Skoda</option>
+                            <option value="Citroen">Citroen</option>
+                            <option value="Fiat">Fiat</option>
+                            <option value="Peugeot">Peugeot</option>
+                            <option value="Chery">Chery</option>
+                            <option value="Great Wall">Great Wall</option>
+                            <option value="Lifan">Lifan</option>
+                            <option value="Haval">Haval</option>
+                            <option value="null">Нет в списке вашего авто?</option>
+                        </select>
+                    </div>
+                    <div class="overflow-hidden">
+                        <transition 
+                            enter-active-class="animate__animated animate__fadeInDown"
+                            leave-active-class="animate__animated animate__fadeOutUp"
+                        >
+                            <div class="form-group" v-if="requestData.brand && requestData.brand == 'null'">
+                                <label for="marka">Напишите марку автомобиля</label>
+                                <input type="text" name="marka" id="marka" class="form-control" v-model="requestData.brandAdd">
+                            </div>
+                        </transition>
+                    </div>
+                    <div class="form-group mb-2">
+                        <label for="model">Введите модель</label>
+                        <input type="text" name="model" id="model" class="form-control" v-model="requestData.model">
+                    </div>
+                    <button class="btn btn-primary" @click="step++" :disabled="(requestData.brand || requestData.brandAdd) && requestData.model ? false : true" >Дальше</button>
                 </div>
             </div>
-            <div class="request-step" v-else-if="step == 3" key="3">3 step</div>
+            <div class="request-step" v-else-if="step == 3" key="3">
+                <p>Выберите из списка ниже</p>
+                <div class="form-check mb-2">
+                    <input class="form-check-input" type="checkbox" value="Проблемы с документами" id="docs" v-model="requestData.additional.doc">
+                    <label class="form-check-label" for="docs">
+                        Проблемы с документами
+                    </label>
+                </div>
+                <div class="form-check mb-2">
+                    <input class="form-check-input" type="checkbox" value="Проблемы с документами" id="repair" v-model="requestData.additional.repair">
+                    <label class="form-check-label" for="repair">
+                        Требуется ремонта авто
+                    </label>
+                </div>
+                <div class="form-check mb-2">
+                    <input class="form-check-input" type="checkbox" value="Проблемы с документами" id="dead" v-model="requestData.additional.dead">
+                    <label class="form-check-label" for="dead">
+                        Битый или не на ходу
+                    </label>
+                </div>
+                <div class="form-check mb-2">
+                    <input class="form-check-input" type="checkbox" value="Проблемы с документами" id="credit" v-model="requestData.additional.credit">
+                    <label class="form-check-label" for="credit">
+                        Кредитный автомобиль
+                    </label>
+                </div>
+                <div class="form-check mb-2">
+                    <input class="form-check-input" type="checkbox" value="Проблемы с документами" id="zalog" v-model="requestData.additional.zalog">
+                    <label class="form-check-label" for="zalog">
+                        Авто находиится в залоге
+                    </label>
+                </div>
+                <button @click="step++" class="btn btn-primary">Дальше</button>
+            </div>
+            <div class="request-step" v-else-if="step == 4" key="4">
+               <p>Спасибо, что ответили на все вопросы, продолжите беседу с нашим менеджером в мессенджере WhatsApp</p>
+               <div class="d-flex justify-content-center">
+                    <button class="wa-button">
+                        <i class="fab fa-whatsapp fa-2x"></i>
+                        Перейти в WhatsApp
+                    </button>
+               </div>
+            </div>
         </transition>
     </div>
 </template>
 
 <style lang="sass">
+.wa-button
+    background: $color-whatsapp
+    color: $color-white
+    border: none
+    display: flex
+    align-items: center
+    justify-content: center
+    padding: 12px
+    border-radius: $default-radius
+    i
+        margin-right: 12px
 .rating-request
     position: relative
     width: 100%
